813d658197d749ee25c9be3bbd6aa17dbcfe3fb5
Léo Maradan <leomaradan@gmail.com>
2016-09-22 18:11:05 +0200

    add "first" on Page request + refactor

--- app/Http/Controllers/Admin/AdminPageController.php
+++ app/Http/Controllers/Admin/AdminPageController.php
@@ -1,13 +1,11 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests;
 use App\Http\Requests\EditPageRequest;
 use App\Http\Controllers\Admin\Controller;
-
 use App\Models\Page;
-
 use Session;
 
 class AdminPageController extends Controller {
@@ -17,23 +15,21 @@ class AdminPageController extends Controller {
      *
      * @return \Illuminate\Http\Response
      */
-	public function index()
-	{
-		$pages = Page::paginate(10);
-		return view('backend.pages.index', compact('pages'));
-	}
+    public function index() {
+        $pages = Page::paginate(10);
+        return view('backend.pages.index', compact('pages'));
+    }
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-	public function create()
-	{
-		$page = new Page();
-		$form = ['url' => route('admin.pages.store')];
-		return view('backend.pages.create', compact('page', 'form'));
-	}
+    public function create() {
+        $page = new Page();
+        $form = ['url' => route('admin.pages.store')];
+        return view('backend.pages.create', compact('page', 'form'));
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -41,15 +37,13 @@ class AdminPageController extends Controller {
      * @param  App\Http\Requests\EditPageRequest  $request
      * @return \Illuminate\Http\Response
      */
-	public function store(EditPageRequest $request)
-	{
-		$page = Page::create($request->all());
+    public function store(EditPageRequest $request) {
+        $page = Page::create($request->all());
 
-		Session::flash('success', "La page a bien été sauvegardé");
+        Session::flash('success', "La page a bien été sauvegardé");
 
-		return redirect(route('admin.pages.create', $page));		
-	}
-
+        return redirect(route('admin.pages.create', $page));
+    }
 
     /**
      * Show the form for editing the specified resource.
@@ -57,12 +51,11 @@ class AdminPageController extends Controller {
      * @param string $slug
      * @return \Illuminate\Http\Response
      */
-	public function edit($slug)
-	{
-		$page = Page::findBySlug($slug);
-		$form = ['method' => 'put', 'url' => route('admin.pages.update', $page)];
-		return view('backend.pages.edit', compact('page', 'form'));
-	}
+    public function edit($slug) {
+        $page = Page::findBySlug($slug)->first();
+        $form = ['method' => 'put', 'url' => route('admin.pages.update', $page)];
+        return view('backend.pages.edit', compact('page', 'form'));
+    }
 
     /**
      * Update the specified resource in storage.
@@ -71,17 +64,15 @@ class AdminPageController extends Controller {
      * @param string $slug
      * @return \Illuminate\Http\Response
      */
-	public function update(EditPageRequest $request, $slug)
-	{
-		$page = Page::findBySlug($slug);
+    public function update(EditPageRequest $request, $slug) {
+        $page = Page::findBySlug($slug)->first();
 
-		$page->update($request->all());
+        $page->update($request->all());
 
-		Session::flash('success', "La page a bien été sauvegardé");
+        Session::flash('success', "La page a bien été sauvegardé");
 
-		return redirect(route('admin.pages.edit', $page));
-
-	}
+        return redirect(route('admin.pages.edit', $page));
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -90,12 +81,11 @@ class AdminPageController extends Controller {
      * @param App\Http\Requests\EditPageRequest  $request
      * @return \Illuminate\Http\Response
      */
-	public function destroy($slug, EditPageRequest $request)
-	{
-		$page = Page::findBySlug($slug);
-		$id = $page->id;
-		$page->delete();
-		return response()->json(['id' => $id])->setCallback($request->input('callback'));
-	}
+    public function destroy($slug, EditPageRequest $request) {
+        $page = Page::findBySlug($slug)->first();
+        $id = $page->id;
+        $page->delete();
+        return response()->json(['id' => $id])->setCallback($request->input('callback'));
+    }
 
 }
